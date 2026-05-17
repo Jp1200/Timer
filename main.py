1d11815ee0d2fac017d3a338205b38571f215f7f
Jack Parker <jprkr96@gmail.com>
2026-05-17 16:18:42 -0500

    stuff

--- main.py
+++ main.py
@@ -5,5 +5,29 @@ import random
 import time
 from PySide6 import QtCore, QtWidgets, QtGui
 
+class Timer(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.hello = ["hello world"]
+        self.button = QtWidgets.QPushButton("Click Me")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
 
+        self.button.clicked.connect(self.magic)
 
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = Timer()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
